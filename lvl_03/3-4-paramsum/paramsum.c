b1cec5778db05ac49a60bd2b999bdb088ee53828
Oleg FEDORYCH <ofedoryc@e1r6p8.unit.ua>
2018-10-23 19:53:15 +0300

    add paramsum.c

---
+++ lvl_03/3-4-paramsum/paramsum.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/23 10:02:46 by exam              #+#    #+#             */
+/*   Updated: 2018/10/23 13:22:34 by exam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 10)
+	{
+		nb = nb + 48;
+		write(1, &nb, 1);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+int		main(int a_c, char **a_v)
+{
+	if (a_v)
+	{
+		if (a_c > 1)
+		{
+			ft_putnbr(a_c - 1);
+			write(1, "\n", 1);
+		}
+		else
+			ft_putstr("0\n");
+	}
+	return (0);
+}
